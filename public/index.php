7596221e00fdbbab35e6111ae5532f4f602bfb76
ConesaJM <javier07062000@gmail.com>
2025-11-07 19:35:48 +0100

    css y correción html

--- public/index.php
+++ public/index.php
@@ -27,46 +27,49 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
-  <title>Inicio de sesión</title>
-</head>
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
+      <link rel="stylesheet" href="style.css">
 
-<!--Formulario -->
-<body>
-    <h1>Introduzca sus credenciales</h1><br><br>
+      <title>Inicio de sesión</title>
+  </head>
 
-    <?php if (!empty($error)): ?>
-      <div class="error"><?= htmlspecialchars($error) ?></div>
-    <?php endif; ?>
+  <!--Formulario -->
+  <body>
+      <h1>Introduzca sus credenciales</h1><br><br>
 
-    <label for="user">Usuario</label>
-    <input type="text" id="user" name="user"
-      value="<?= htmlspecialchars($_POST['user'] ?? '') ?>" required><br><br>
+      <form action="" method="post">
+        <?php if (!empty($error)): ?>
+          <div class="error"><?= htmlspecialchars($error) ?></div>
+        <?php endif; ?>
 
-    <label for="password">Contraseña:</label>
-    <input type="password" id="password" name="password"
-      value="<?= htmlspecialchars($_POST['password'] ?? '') ?>" required>
-    <i class="fa-solid fa-eye toggle-password" id="togglePassword"></i><br><br>
+        <label for="user">Usuario</label>
+        <input type="text" id="user" name="user"
+          value="<?= htmlspecialchars($_POST['user'] ?? '') ?>" required><br><br>
 
-    <button type="submit">Iniciar sesión</button>
-  </form>
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" name="password"
+          value="<?= htmlspecialchars($_POST['password'] ?? '') ?>" required>
+        <i class="fa-solid fa-eye toggle-password" id="togglePassword"></i><br><br>
 
-    <script>
-    const togglePassword = document.getElementById('togglePassword');
-    const passwordInput = document.getElementById('password');
+        <button type="submit">Iniciar sesión</button>
+    </form>
 
-    togglePassword.addEventListener('click', () => {
-      const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-      passwordInput.setAttribute('type', type);
-      
-      // Cambia el icono
-      togglePassword.classList.toggle('fa-eye');
-      togglePassword.classList.toggle('fa-eye-slash');
-    });
-  </script>
+      <script>
+      const togglePassword = document.getElementById('togglePassword');
+      const passwordInput = document.getElementById('password');
 
-</body>
+      togglePassword.addEventListener('click', () => {
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+        
+        // Cambia el icono
+        togglePassword.classList.toggle('fa-eye');
+        togglePassword.classList.toggle('fa-eye-slash');
+      });
+    </script>
+
+  </body>
 </html>
