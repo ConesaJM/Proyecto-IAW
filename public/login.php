f5f39537a27e66c75626eeb7226cf48153e0a1b8
ConesaJM <javier07062000@gmail.com>
2025-11-10 20:24:34 +0100

    añadir favicon a la página

--- public/login.php
+++ public/login.php
@@ -24,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
       <link rel="stylesheet" href="style.css">
-
+      <link rel="icon" type="image/x-icon" href="/media/favicon_io/favicon.icon">
       <title>Inicio de sesión</title>
   </head>
 
