37d82e7a5146e0621677c02fe718b7bde6897bc1
alejandroag-06 <alejandroaledog06@gmail.com>
2025-11-10 19:53:13 +0100

    Cambio PDO y readme

--- README.md
+++ README.md
@@ -7,5 +7,18 @@
 # - Alejandro Aledo García
 #
 # Descripción: El proyecto trata de la integración de cookies de inicio de sesión, junto a un inventario donde los usuarios podrán crear productos.
+
+# indice
+# recursos (bloque bd)
+# funcionamiento / tutorial como funciohnq pagina
+# 
+#
+# 
 #
 #
+#
+# IDEA DE README:
+## - Pegar "schema.sql" a MySQL
+## - Pegar "seed.sql" a MySQL
+## - Insertar enlace BD a los recursos schema.sql y seed.sql para su correcto funcionamiento. 
+
--- app/pdo.php
+++ app/pdo.php
@@ -6,10 +6,6 @@ $user = "admin";
 $pass = "Admin_IAW_super";
 $dsn  = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
-// Credenciales usuario
-$user2 = "usuario";
-$pass2 = "Usuario_IAW_super";
-
 try {
     $pdo = new PDO($dsn, $user, $pass, [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
@@ -19,16 +15,5 @@ try {
     echo "Conexión admin OK<br>";
 } catch (PDOException $e) {
     die("Error de conexión BD (admin): " . $e->getMessage());
-}
-
-try {
-    $pdo2 = new PDO($dsn, $user2, $pass2, [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-    ]);
-    echo "Conexión usuario OK<br>";
-} catch (PDOException $e) {
-    die("Error de conexión BD (usuario): " . $e->getMessage());
 }
 ?>
