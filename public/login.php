62ae127224415b7a9a317e17d7e5b67df9b99bbc
ConesaJM <javier07062000@gmail.com>
2025-11-10 21:45:54 +0100

    correción de favicon y eliminación de imagenes no importantes

--- public/login.php
+++ public/login.php
@@ -24,8 +24,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
       <link rel="stylesheet" href="style.css">
-      <link rel="icon" type="image/x-icon" href="/media/favicon_io/favicon.icon">
-      <title>Inicio de sesión</title>
+      <link rel="icon" href="media/favicon.png" type="image/png" sizes="512x512">
+      <title>Inicio de sesión</title> 
   </head>
 
   <!--Formulario -->
