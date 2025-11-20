cc408284b7d48cb28ddcbfce3800b2943cdba9f8
ConesaJM <javier07062000@gmail.com>
2025-11-20 14:15:38 +0100

    comentarios

--- public/logout.php
+++ public/logout.php
@@ -1,8 +1,12 @@
 <?php
 require_once __DIR__ . '/../app/auth.php';
 
+// Vaciar variables de sesión
 $_SESSION = [];
+
+// Destruir la sesión
 session_destroy();
 
+// Redirección a login
 header('Location: login.php');
 exit;
