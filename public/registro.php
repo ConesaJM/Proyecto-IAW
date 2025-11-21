9dae14cf3db31008249883410d98fe05fab33f4c
ConesaJM <javier07062000@gmail.com>
2025-11-21 11:08:56 +0100

    formulario basico

--- public/registro.php
+++ public/registro.php
@@ -18,7 +18,7 @@ if (isset($_SESSION['user_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PharmaSphere - Inicio de Sesión</title>
+    <title>harmaSphere - Registro</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
@@ -34,6 +34,29 @@ if (isset($_SESSION['user_id'])) {
             <h1 class="brand-title">PharmaSphere</h1>
             <p class="brand-subtitle">Sistema de Gestión Farmacéutica</p>
         </div>
+
+         <div class="form-section">
+            <h2>Crear cuenta</h2>
+
+            <form method="post">
+                <label for="nombre">Nombre de usuario</label><br>
+                <input type="text" id="nombre" name="nombre"><br><br>
+
+                <label for="password">Contraseña</label><br>
+                <div style="position:relative; display:inline-block;">
+                    <input type="password" id="password" name="password">
+                    <i class="fa-solid fa-eye" id="togglePassword"
+                       style="position:absolute; right:8px; top:50%; transform:translateY(-50%); cursor:pointer;"></i>
+                </div>
+                <br><br>
+
+                <button type="submit">Crear usuario</button>
+            </form>
+
+            <br>
+            <a href="login.php">Volver al login</a>
+        </div>
+
     </div>
 
         <!-- Script para ver/ocultar contraseña -->
